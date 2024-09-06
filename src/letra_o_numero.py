2f91bd40201746d4d7fa9687a183c8e2e692e280
Yenifer Leon <neutronwar34@gmail.com>
2024-09-06 11:02:15 -0500

    Actividad letra o numero

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,6 +1,19 @@
 def evaluar(caracter):
-    # TODO: Coloca aquí el código del ejercicio 4: Letra o número
-    return "";
+    if len(caracter) >1:
+        return "entrada no permitida"
+    if caracter.isalpha():
+ 
+        if caracter.isupper():
+            return "Es letra mayúscula"
+        
+        elif caracter.islower():
+            return "Es letra minúscula"
+        
+    elif caracter.isdigit():
+        return "Es número"
+    
+    else:
+        return "No es letra ni número"
 
 if __name__ == '__main__':
     print("Caracter:", end='')
