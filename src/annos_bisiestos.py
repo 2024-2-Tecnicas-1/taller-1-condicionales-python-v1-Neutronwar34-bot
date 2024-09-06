a63b72d223868546fdba880c3ebcd327d2947c1e
Yenifer Leon <neutronwar34@gmail.com>
2024-09-06 09:58:05 -0500

    Se  subio solución problema 2, años bisiestos

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,7 +1,25 @@
 def evaluar(anno):
-    # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    return "";
-
+    #Calendario Juliano
+    if anno < 1582:
+        if anno % 4 == 0:
+            return str(anno) + " es bisiesto"
+        else:
+            return str(anno) + " no es bisiesto"
+        
+    #Calendario Gregoriano
+    else:
+        if anno % 4 == 0:
+            if anno % 100 == 0:
+                if anno % 400 == 0:
+                    return str(anno) + " es bisiesto"
+                else:
+                    return str(anno) + " no es bisiesto"
+            else:
+                return str(anno) + " es bisiesto"
+        else:
+            return str(anno) + " no es bisiesto"
+        
+        
 if __name__ == '__main__':
     print("Año:", end="")
     anno = int(input())
